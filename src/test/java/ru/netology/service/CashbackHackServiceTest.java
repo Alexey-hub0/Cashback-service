2fbbee04dacd95116bdbcca07133e15aff43b67e
Alexey Test <yashin.aleksey.job@gmail.com>
2023-10-02 15:14:34 +0300

    testng commit2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,41 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
+    @Test
+    void cashbackServiceAmount900(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void cashbackServiceAmount0(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void cashbackServiceAmount1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
